f97d0c66257757bc2a61295a0573e09024c9cebf
Émile <69686396+Kkiriya@users.noreply.github.com>
2026-08-13 18:01:41 -0400

    addded prisma technical requirements

---
+++ server/prisma/seed.ts
@@ -0,0 +1,9 @@
+// Generates a credible data set
+// at least 10 users, inculding 1 admin
+// at least 30 days of journal entry
+// multiple groups, including one private
+// around 20 resources of varying categories
+// enough post so that pagination is visible
+
+// handle unicitity problems
+
