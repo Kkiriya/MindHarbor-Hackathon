423dce33e0e975847dbcf0668d9add2c9da650ea
Jean-Simon Cyr <jsimcyr@gmail.com>
2026-08-14 11:02:30 -0400

    feat: refactor Header layout to use grid

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 export default function Header() {
     return (
         <header className={styles.header}>
-            <section className={styles.content}>
+            <div className={styles.content}>
                 <img src="/mindharbor-logo.png" alt="Logo" className={styles.logo}/>
                 <nav className={styles.nav}>
                     <ul>
@@ -14,7 +14,7 @@ export default function Header() {
                         <li><Link to="/analysis" className={styles.Link}>Analyse et tendances</Link></li>
                     </ul>
                 </nav>
-            </section>
+            </div>
         </header>
     )
 }
