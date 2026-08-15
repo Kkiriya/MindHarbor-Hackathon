96919bad18ef7eb6cf55f15a39f19ad7dedd80dc
Jean-Simon Cyr <jsimcyr@gmail.com>
2026-08-14 21:37:27 -0400

    feat: add Personal Dashboard template components for Today, Week, and Notifications

--- client/src/pages/PersonalDashboardPage.tsx
+++ client/src/pages/PersonalDashboardPage.tsx
@@ -1,7 +1,16 @@
+import Today from "../components/personal-dashboard/today.tsx";
+import Week from "../components/personal-dashboard/week.tsx";
+import Notifications from "../components/personal-dashboard/notifcations.tsx";
+
 export default function PersonalDashboardPage() {
     return (
         <main>
-            <h1>Personal Dashboard</h1>
+            <h1>Bonjour</h1>
+            <p>Voici un aperçu de ta semaine</p>
+
+            <Today/>
+            <Week/>
+            <Notifications/>
         </main>
     )
 }
